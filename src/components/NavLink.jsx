cddfc36595ba0bfd4e43905676a96a108d321e76
Romain <romain.herault99@gmail.com>
2022-07-06 14:02:19 +0200

    feat: 404 page and fix links

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,11 +2,8 @@ import Link from 'next/link'
 
 export default function NavLink({ href, children }) {
   return (
-    <Link
-      href={href}
-      className="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
-    >
-      {children}
+    <Link href={href}>
+      <a className='inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900'>{children}</a>
     </Link>
   )
 }
