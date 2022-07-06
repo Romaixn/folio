0a064285408843d7fc23a2b5f5109257f3bb2501
Romain <romain.herault99@gmail.com>
2022-07-06 11:24:45 +0200

    fix: no head in document

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,11 @@
-import { Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document(props) {
   let pageProps = props.__NEXT_DATA__?.props?.pageProps
 
   return (
     <Html className="h-full" lang="fr">
+      <Head />
       <body className="h-full">
         <Main />
         <NextScript />
