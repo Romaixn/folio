752e9f5db3635921d5c9cf49e4a5999db3aa8f4f
Romain <romain.herault99@gmail.com>
2022-07-06 12:01:55 +0200

    feat: change primary color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
       <Container className="pb-16 sm:pb-24">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block text-indigo-600">
+            <span className="block text-red-600">
                 Romain Herault
             </span>{' '}
             <span className="block">Développeur web full stack</span>
@@ -19,7 +19,7 @@ export default function Hero() {
             <div className="rounded-md shadow">
               <a
                 href="#"
-                className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-8 py-3 text-base font-medium text-white hover:bg-red-700 md:py-4 md:px-10 md:text-lg"
               >
                 Get started
               </a>
@@ -27,7 +27,7 @@ export default function Hero() {
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
               <a
                 href="#"
-                className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-red-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg"
               >
                 Live demo
               </a>
