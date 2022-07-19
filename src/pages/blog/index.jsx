3f13269853a331b80b57b42c2f35a1d40a063c34
Romain <romain.herault99@gmail.com>
2022-07-19 15:30:31 +0200

    feat: add newsletter to blog index

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Date from '@/components/Date'
 import Layout from '@/components/Layout'
 import Container from '@/components/Container'
+import Notification from '@/components/Notification'
 import { getAllPosts } from '@/lib/wordpress/api'
 
 export default function Blog({ posts: { edges } }) {
+  const [notification, setNotification] = useState(null)
+  const [status, setStatus] = useState(null)
+  const [sending, setSending] = useState(false)
+
+  const submitNewsletter = async (event) => {
+    setSending(true)
+    event.preventDefault()
+
+    const email = event.target.email.value
+
+    const res = await fetch('/api/newsletter', {
+      body: JSON.stringify({ email }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    setStatus(res.status)
+    notification = await res.json()
+    setNotification(notification)
+    event.target.reset()
+    setSending(false)
+  }
+
   return (
     <Layout>
       <Head>
@@ -19,7 +46,14 @@ export default function Blog({ posts: { edges } }) {
         aria-hidden="true"
         className="absolute inset-x-0 top-0 h-16 bg-gradient-to-b from-gray-50"
       ></div>
-      <Container className="bg-white pt-8 sm:pt-16 pb-20 lg:pb-28">
+      {notification && (
+        <Notification
+          title={status === 201 ? "Merci pour votre inscription !" : "Oups, une erreur s'est produite !"}
+          status={status}
+          message={notification}
+        />
+      )}
+      <Container className="bg-white pt-8 pb-20 sm:pt-16 lg:pb-28">
         <div className="relative mx-auto max-w-lg divide-y-2 divide-gray-200 lg:max-w-7xl">
           <div>
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -30,14 +64,14 @@ export default function Blog({ posts: { edges } }) {
                 Inscris-toi pour recevoir ma veille technologique, toutes les
                 semaines et t'améliorer en tant que développeur.
               </p>
-              <form className="mt-6 flex flex-col sm:flex-row lg:mt-0 lg:justify-end">
+              <form className="mt-6 flex flex-col sm:flex-row lg:mt-0 lg:justify-end" onSubmit={submitNewsletter}>
                 <div>
-                  <label htmlFor="email-address" className="sr-only">
+                  <label htmlFor="email" className="sr-only">
                     Adresse email
                   </label>
                   <input
-                    id="email-address"
-                    name="email-address"
+                    id="email"
+                    name="email"
                     type="email"
                     autoComplete="email"
                     required
@@ -48,7 +82,8 @@ export default function Blog({ posts: { edges } }) {
                 <div className="mt-2 flex w-full flex-shrink-0 rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:inline-flex sm:w-auto">
                   <button
                     type="button"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:inline-flex sm:w-auto"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:inline-flex sm:w-auto disabled:opacity-80"
+                    {...(sending ? { disabled: true } : {})}
                   >
                     S'inscrire
                   </button>
@@ -76,7 +111,7 @@ export default function Blog({ posts: { edges } }) {
                 <div className="mt-3">
                   <Link href={`/blog/${node.slug}`}>
                     <a className="text-base font-semibold text-red-600 hover:text-red-500">
-                        Lire l'article
+                      Lire l'article
                     </a>
                   </Link>
                 </div>
