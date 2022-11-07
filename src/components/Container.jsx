8f3010f312b45ccd59558c2f27c4199b8f5524da
Romain <romain@rherault.fr>
2022-11-07 18:16:12 +0100

    feat: redesign of portfolio

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,42 @@
+import { forwardRef } from 'react'
 import clsx from 'clsx'
 
-export default function Container({ className, ...props }) {
+const OuterContainer = forwardRef(function OuterContainer(
+  { className, children, ...props },
+  ref
+) {
+  return (
+    <div ref={ref} className={clsx('sm:px-8', className)} {...props}>
+      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+    </div>
+  )
+})
+
+const InnerContainer = forwardRef(function InnerContainer(
+  { className, children, ...props },
+  ref
+) {
   return (
     <div
-      className={clsx('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)}
+      ref={ref}
+      className={clsx('relative px-4 sm:px-8 lg:px-12', className)}
       {...props}
-    />
+    >
+      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+    </div>
   )
-}
+})
+
+export const Container = forwardRef(function Container(
+  { children, ...props },
+  ref
+) {
+  return (
+    <OuterContainer ref={ref} {...props}>
+      <InnerContainer>{children}</InnerContainer>
+    </OuterContainer>
+  )
+})
+
+Container.Outer = OuterContainer
+Container.Inner = InnerContainer
