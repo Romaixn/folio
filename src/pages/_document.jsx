cb43c75fec1485b6f175492469500d7729db8976
rherault <romain@rherault.dev>
2023-03-07 09:49:33 +0100

    feat: customize greve banner

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -8,6 +8,26 @@ export default function Document(props) {
       <Head />
       <body className="h-full">
         <Main />
+        <script type="text/javascript">
+          var DIGITAL_STRIKE_OPTIONS = {
+            websiteName: 'rherault.fr',
+            /**
+             * Afficher le widget en mode footer en dehors des dates prévues (voir fullPageDisplayStartDate)
+             * false par défault: affichage en mode "full page" tous les jours
+             */
+            minMode: true, // @type {boolean}
+            /**
+             * Date à partir de laquelle doit s'afficher le widget en mode footer
+             * ⚠️ Janvier = 0, Décembre = 11 (mois - 1)
+             */
+            footerDisplayStartDate: new Date(2023, 2, 8), //@ type {Date object}
+
+            /**
+             * En mode min, date à laquelle le footer doit s'afficher en pleine page, pour 24 heures. 
+             */
+            fullPageDisplayStartDate: new Date(2023, 2, 7), //@ type {Date object}
+          };
+        </script>
         <script src="https://onestla.tech/widget-engreve/widget.js" async></script>
         <NextScript />
       </body>
